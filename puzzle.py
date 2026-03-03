3126d0e7bf372dc6221b527838c8f50ed95210e3
Rachel Papirmeister <rmp2205@columbia.edu>
2026-03-02 21:24:02 -0500

    DFS algo

--- puzzle.py
+++ puzzle.py
@@ -75,3 +75,31 @@ def bfs(start_board):
                     max_depth = child.depth
 
     return None, nodes_expanded, max_depth
+
+def dfs(start_board):
+    start_state = PuzzleState(start_board)
+    frontier = [start_state]
+    frontier_set = {start_board}
+    explored = set()
+    nodes_expanded = 0
+    max_depth = 0
+
+    while frontier:
+        state = frontier.pop()
+        frontier_set.discard(state.board)
+
+        if state.board == GOAL_STATE:
+            return state, nodes_expanded, max_depth
+
+        explored.add(state.board)
+        nodes_expanded += 1
+
+        for direction, new_board in reversed(state.get_neighbors()):
+            if new_board not in explored and new_board not in frontier_set:
+                child = PuzzleState(new_board, parent=state, move=direction, depth=state.depth + 1)
+                frontier.append(child)
+                frontier_set.add(new_board)
+                if child.depth > max_depth:
+                    max_depth = child.depth
+
+    return None, nodes_expanded, max_depth
